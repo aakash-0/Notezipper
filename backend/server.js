2d3c1883544d3dac2423e639dd50d5976383d4c2
Aakash Yadav <yaakash59.ay@gmail.com>
2022-08-26 10:26:35 +0530

    Enviroment setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config()
+
+const PORT = process.env.PORT||5000;
+
+
+app.listen(PORT,()=>{
+    console.log("Running server on port 5000")
+})
